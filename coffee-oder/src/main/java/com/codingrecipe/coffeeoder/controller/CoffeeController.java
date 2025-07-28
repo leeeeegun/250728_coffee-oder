a73d67ebf56d438de256eeb67e60b95dcb3a5eae
leeeeegun <leeeeegun@naver.com>
2025-07-28 10:14:59 +0900

    feat: [이의건] Controller 구성 [CO-1]

---
+++ coffee-oder/src/main/java/com/codingrecipe/coffeeoder/controller/CoffeeController.java
@@ -0,0 +1,36 @@
+package com.codingrecipe.coffeeoder.controller;
+
+
+import jakarta.servlet.ServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/coffee")
+public class CoffeeController {
+
+    private final CoffeeService coffeeService;
+
+    // 커피 등록
+    // 요청 URI -> /coffee/register
+    // 인증 헤더 -> @RequestHeader
+    // 요청 정보 -> @RequestBody
+
+    @PostMapping("/coffee/register")
+    public RegisterCoffeeResponseForm registerCoffee(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestBody RegisterCoffeeResponseForm requestForm, ServletResponse servletResponse) {
+
+        // 인증 정보 체크 ("bearer 떼어내기")
+        String userToken = authorizationHeader.replace("Bearer" , " ").trim();
+        Long accountId = redisCacheService.getValueByKey(userToken, Long.class);
+
+        RegisterCoffeeRequest request = requestForm.toRigsterRequest();
+        RegisterCoffeeResponse = coffeeService.register(request, accountId);
+
+        return RegisterCoffeeResponseForm.from(response);
+    }
+}
