6eff356f9d459498cf33c9a52c33ea33b41f1995
leeeeegun <leeeeegun@naver.com>
2025-07-28 10:39:37 +0900

    feat: [이의건] Coffee Service Register Request 구성 [CO-4]

---
+++ coffee-oder/src/main/java/com/codingrecipe/coffeeoder/service/request/RegisterCoffeeRequest.java
@@ -0,0 +1,16 @@
+package com.codingrecipe.coffeeoder.service.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RegisterCoffeeRequest {
+    private String coffeeName;
+    private String coffeePrice;
+    private String coffeeData;
+
+    public Coffee toCoffee(Account account) {
+        return new Coffee (coffeeName, coffeePrice, coffeeData, account);
+    }
+}
