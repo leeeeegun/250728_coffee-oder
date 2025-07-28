1ffd6a9692f4e63a6ee2300fc45e11fe17945c95
leeeeegun <leeeeegun@naver.com>
2025-07-28 10:12:48 +0900

    Initial commit

---
+++ coffee-oder/src/main/java/com/codingrecipe/coffeeoder/CoffeeOderApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.coffeeoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOderApplication.class, args);
+    }
+
+}
---
+++ coffee-oder/src/test/java/com/codingrecipe/coffeeoder/CoffeeOderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.coffeeoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
