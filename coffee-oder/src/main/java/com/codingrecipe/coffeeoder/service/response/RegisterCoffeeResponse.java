d26f3c7704c658431715dc5a10e7562697167c63
leeeeegun <leeeeegun@naver.com>
2025-07-28 10:44:46 +0900

    feat: [이의건] Coffee Service Register Response 구성 [CO-5]

---
+++ coffee-oder/src/main/java/com/codingrecipe/coffeeoder/service/response/RegisterCoffeeResponse.java
@@ -0,0 +1,24 @@
+package com.codingrecipe.coffeeoder.service.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RegisterCoffeeResponse {
+    private final Long CoffeeId;
+    private final String CoffeeName;
+    private final String CoffeePrice;
+    private final String CoffeeData;
+    private final String registerAccountNickname;
+
+    public RegisterCoffeeResponse from(Coffee coffee) {
+        return new RegisterCoffeeResponse(
+                coffee.getId(),
+                coffee.getName(),
+                coffee.getPrice(),
+                coffee.getData(),
+                coffee.getAccount().getNickname()
+        );
+    }
+}
