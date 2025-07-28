2062fabc43b81713cf44d3d8c850520f5b3b235d
leeeeegun <leeeeegun@naver.com>
2025-07-28 12:22:27 +0900

    feat: [이의건] Account Controller 구성 [CO-13]

--- coffee-oder/src/main/java/com/codingrecipe/account/controller/AccountController.java
+++ coffee-oder/src/main/java/com/codingrecipe/account/controller/AccountController.java
@@ -1,4 +1,48 @@
 package com.codingrecipe.account.controller;
 
+import com.codingrecipe.redis_cache.RedisCacheService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/account")
 public class AccountController {
+
+    final private AccountService accountService;
+    final private RedisCacheService redisCacheService;
+
+    @PostMapping("/register")
+    public RegisterAccountResponseForm createAccount(
+            @RequestHeader("Authorization") String authorizedHeader,
+            @RequestBody RegisterAccountRequestForm requestForm) {
+
+        // 지금 가입을 요청한 사용자가 정상적인 임시 토큰을 가지고 있는가 ?
+        String temporaryUserToken = authorizedHeader.replace("Bearer ", "");
+        String accessToken = redisCacheService.getValueByKey(temporaryUserToken, String.class);
+
+        if (accessToken == null) {
+            throw new IllegalArgumentException("만료되었거나 잘못된 토큰 요청입니다");
+        }
+
+        // 회원 가입 진행
+        RegisterAccountRequest request = requestForm.toRegisterAccountRequest();
+        RegisterAccountResponse response = accountService.register(request);
+        Long accountId = response.getAccountId();
+
+        // 토근 발급
+        String userToken = UUID.randomUUID().toString();
+        redisCacheService.setKeyAndValue(userToken, accountId);
+        redisCacheService.setKeyAndValue(accountId, accessToken);
+
+        // 임시 토큰 삭제
+        redisCacheService.deleteByKey(temporaryUserToken);
+
+        return RegisterAccountResponseForm.from(response, userToken);
+
+    }
 }
