6160097835a3e31688b10ff14b4ad5c98cbd6beb
leeeeegun <leeeeegun@naver.com>
2025-07-28 14:16:03 +0900

    feat: [이의건] Account Service Request 구성 [CO-18]

---
+++ coffee-oder/src/main/java/com/codingrecipe/account/service/request/RegisterAccountRequest.java
@@ -0,0 +1,15 @@
+package com.codingrecipe.account.service.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RegisterAccountRequest {
+    final private String email;
+    final private String nickname;
+
+    public Account toAccount() {
+        return new Account(email, nickname);
+    }
+}
