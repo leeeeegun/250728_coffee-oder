ab8fe0aac8ea659268ca0fa7f6eddc5fd8f82347
leeeeegun <leeeeegun@naver.com>
2025-07-28 14:17:26 +0900

    feat: [이의건] Account Repository 구성 [CO-19]

---
+++ coffee-oder/src/main/java/com/codingrecipe/account/repository/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.codingrecipe.account.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class AccountRepository extends JpaRepository<Account, Long> {
+}
