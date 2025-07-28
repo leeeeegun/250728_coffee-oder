4cbbf927f682d4037e296ad11f0195357f5cbb51
leeeeegun <leeeeegun@naver.com>
2025-07-28 12:27:48 +0900

    feat: [이의건] Account Controller ResponseForm 구성 [CO-14]

---
+++ coffee-oder/src/main/java/com/codingrecipe/account/controller/response_form/RegisterAccountResponseForm.java
@@ -0,0 +1,24 @@
+package com.codingrecipe.account.controller.response_form;
+
+import com.codingrecipe.coffee_oder.controller.request_form.RegisterCoffeeRequestForm;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RegisterAccountResponseForm {
+    final private String userToken;
+    final private String email;
+    final private String nickname;
+
+    public static RegisterAccountRequestForm from(
+            final RegisterAccountResponse response,
+            final String userToken) {
+
+        return new RegisterAccountResponseForm(
+                userToken,
+                response.getEmail(),
+                response.getNickname()
+        );
+    }
+}
